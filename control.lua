ca3d97ed007fdd0be34bec2e9eeabc1f59efadc6
slikts <dabas@untu.ms>
2016-04-12 10:44:02 +0300

    add remote commands

--- control.lua
+++ control.lua
@@ -12,9 +12,15 @@ local tf = require('./lib/timeflaps')(config, _log, global)
 
 remote.add_interface('timeflaps', {
   set_config = function(k, v)
-    config[k] = v
-    _log('set config', k, v)
+    local _v = loadstring('return ' .. v)()
+    config[k] = _v
+    _log('set config', k, serpent.line(_v))
   end,
+  pos = function()
+    game.player.print(string.format('player position: { %d, %d }', game.player.position.x, game.player.position.y))
+  end,
+  cap_start = tf.cap_start,
+  cap_stop = tf.cap_stop,
 })
 
 if config.cap_autostart then
